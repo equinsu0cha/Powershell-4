e64f6f456ccf389e8af34b35f53f1924e0ea61c3
matemohanish <mate.mohanish@gmail.com>
2018-06-28 11:58:30 +0530

    Add files via upload

---
+++ Custom FIle Movement.ps1
@@ -0,0 +1,16 @@
+﻿Help Rename-Item -Examples
+
+
+$dates = (Get-date).ToString("dd-MM-yyyy hh-mm")
+New-Item -Path C:\Temp -Name $dates -ItemType Directory
+
+Copy-Item C:\Temp\* -Destination C:\Temp\$dates
+$files =Get-ChildItem -Path C:\Temp\$dates| select name
+
+foreach($f in $files.name){
+    If ($f -like "*Server*"){
+        Rename-Item -Path "C:\Temp\$dates\28-06-2018 10-48NewServers.txt" `
+        -NewName ($dates +'-'+ "NewServers.txt")
+
+    }
+}
---
+++ reg-SoftwareInventory.ps1
@@ -0,0 +1,5 @@
+﻿$path1 ="HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+$path2 ="HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+
+Get-ItemProperty -Path $path1,$path2 | Where {$_.displayname}| Where {$_.Publisher -notlike "*Microsoft*"}|
+                            Select Displayname, Displayversion,Publisher | Out-GridView
