b164080018cccebbbc74fa8e550b38ce9c1ece49
matemohanish <mate.mohanish@gmail.com>
2018-06-28 09:43:43 +0530

    Add files via upload

---
+++ SoftwareInventory.ps1
@@ -0,0 +1,17 @@
+﻿#$servers = Get-Content -path C:\Temp\Servers.txt
+$servers = "Localhost"
+foreach($server in $servers){
+ $testpath = Test-path \\$server\c$\windows
+     If ($testpath){
+         Get-wmiobject -Class win32_product -ComputerName $server | 
+         Where {$_.vendor -ne "Microsoft corporation"} | Export-Csv C:\Temp\Softwares.csv
+         #ConvertTo-Html -Head "Software Inventory"|
+         #Out-file c:\temp\Softwareinventory.htm
+         #Get-wmiobject -Class win32_product -ComputerName $server | Where {$_.vendor -eq "Adobe corporation"}
+     } 
+     Else 
+     {
+        $server |out-file  C:\temp\InAccessibleservers.txt
+     }
+ 
+}
