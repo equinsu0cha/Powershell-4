85f0d3dd6c8f8058306f33f13d9e0809fcc09254
PSCloudmaster <psmohnish@gmail.com>
2018-07-20 12:47:54 +0530

    Add files via upload

---
+++ Credential Automation.ps1
@@ -0,0 +1,6 @@
+﻿<#Comment below line after successfully recording the password in Pass.txt file. 
+This is not the most secure way of passing password.#>
+$credential = Get-Credential
+$credential.Password | ConvertFrom-SecureString | Set-Content c:\temp\pass.txt
+$Password = Get-Content c:\temp\pass.txt | ConvertTo-SecureString
+$credential = New-Object System.Management.Automation.PsCredential($username, $Password)
