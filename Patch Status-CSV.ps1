8bf136e1f1a9aaaeb65e9fb7b1faeb7a560d02c9
PSCloudmaster <psmohnish@gmail.com>
2018-08-09 21:03:23 +0530

    Add files via upload

---
+++ Patch Status-CSV.ps1
@@ -0,0 +1,22 @@
+﻿$servers = Get-Content C:\Temp\Servers.txt
+
+foreach($server in $servers){
+    $Patches = Gwmi Win32_QuickFixEngineering -ComputerName $Server
+    $OS = Gwmi Win32_Operatingsystem -ComputerName $server 
+    foreach ($patch in $Patches)
+    {
+    $Hash = @{
+
+            Computername = $os.__Server;
+            OSName = ($os.Name).Split("|")[0]
+            Architecture = $OS.OSArchitecture
+            InstalledDate= $Patch.InstalledOn;
+            HotFixID=$Patch.HotFixID;
+            Description=$Patch.Description;
+            InstalledBy=$Patch.InstalledBy
+            }
+
+    $OBJ = New-Object -TypeName PSObject -Property $Hash
+    $OBJ | Export-csv C:\Temp\Patch.csv -Append -NoTypeInformation
+    }
+}
